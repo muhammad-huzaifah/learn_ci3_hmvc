5793159bfa833a2c8f83193bf8e873afe2c3cc43
ujebb <ujebbawas@yahoo.com>
2022-09-19 14:58:13 +0700

    learn ci3 hmvc

---
+++ application/modules/produk/views/vw_produk.php
@@ -0,0 +1,9 @@
+<span style="color: #ff0000"><html></span>
+<head>
+	<meta charset="UTF-8">
+	<title>Belajar HMVC di Codeigniter</title>
+</head>
+<body>
+	<h1>Ini adalah halaman module Produk</h1>
+</body>
+</html>
